6ec4190be7c98ea7f743d93affef3654ca101065
Eliran Peer <bluzi@Elirans-Air.lan>
2021-07-13 15:44:28 +0300

    run postinstall script only if needed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
 const { existsSync } = require('fs')
 const { resolve } = require('path')
 
-const { readDependencies, isLocalPath } = require('./utils')
+const { readDependencies, isLocalPath, runNpmScript, install } = require('./utils')
 
 const prefix = 'pack-local-dependencies:'
-const commands = [
-  'npm i',
-  `npm run ${prefix}postinstall`,
-]
 
 const dependencies = readDependencies()
 const devDependencies = readDependencies('devDependencies')
@@ -19,20 +14,13 @@ Object.entries({ ...dependencies, ...devDependencies })
   .filter(([, version]) => isLocalPath(version))
   .forEach(([dependency, localPath]) => {
     const absolutePath = resolve(localPath.replace('file:', ''))
+    
     if (!existsSync(absolutePath)) {
       console.warn(`${dependency}: Could not find package in '${localPath}', assuming it was not installed`)
       return
     }
 
     console.log(`${dependency}: Executing 'npm i' in ${absolutePath}`)
-    commands.forEach((command) => {
-      try {
-        execSync(command, {
-          stdio: 'inherit',
-          cwd: absolutePath,
-        })
-      } catch(e) {
-        console.warn(`${dependency}: command '${command}' exited with code ${e.status}`)
-      }
-    })
+    install(absolutePath)
+    runNpmScript(absolutePath, `${prefix}postinstall`)
   })
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,22 @@
 const { readFileSync, existsSync } = require('fs')
+const { resolve } = require('path')
+const { execSync } = require('child_process')
 
-const readDependencies = (key = 'dependencies') => {
-  const packageJsonPath = './package.json'
+const readPackage = (path = './') => {
+  const packagePath = resolve(path, 'package.json')
 
-  if (!existsSync(packageJsonPath)) {
+  if (!existsSync(packagePath)) {
     throw new Error(`Could not find package.json in ${process.cwd()}`)
   }
 
-  const packageJsonContents = readFileSync('package.json', 'utf-8')
-  const packageJson = JSON.parse(packageJsonContents)
+  const packageContents = readFileSync(packagePath, 'utf-8')
+  const package = JSON.parse(packageContents)
+
+  return package
+}
+
+const readDependencies = (key = 'dependencies') => {
+  const packageJson = readPackage()
 
   return packageJson?.[key] || {}
 }
@@ -19,7 +27,39 @@ const isLocalPath = (version) => {
   return false
 }
 
+const runNpmScript = (path, script) => {
+  const packageJson = readPackage(path)
+
+  if (packageJson?.scripts?.[script]) {
+    const command = `npm run ${script}`
+    
+    try {
+      execSync(`npm run ${script}`, {
+        stdio: 'inherit',
+        cwd: path,
+      })
+    } catch (e) {
+      console.warn(`${dependency}: command '${command}' exited with code ${e.status}`)
+    }
+  }
+}
+
+const install = (path) => {
+  const command = `npm i`
+
+  try {
+    execSync(command, {
+      stdio: 'inherit',
+      cwd: absolutePath,
+    })
+  } catch (e) {
+    console.warn(`${dependency}: command '${command}' exited with code ${e.status}`)
+  }
+}
+
 module.exports = {
   readDependencies,
   isLocalPath,
+  runNpmScript,
+  install,
 }
